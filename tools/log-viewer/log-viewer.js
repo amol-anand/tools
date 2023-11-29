1d5f0207d3faffdecc84ba31f06d9cf5215a84d0
Amol Anand <amol.anand@gmail.com>
2023-11-28 21:27:13 -0500

    initial log viewer functionality

---
+++ tools/log-viewer/log-viewer.js
@@ -0,0 +1,107 @@
+const regexpFull = /^.*github.com\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)\/tree\/([a-zA-Z0-9_-]+)$/;
+const regexpPartial = /^.*github.com\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)$/;
+const ghSubmit = document.getElementById('githubsubmit');
+const options = {
+  valueNames: [
+    'timestamp',
+    'status',
+    'method',
+    'route',
+    'path',
+    'user',
+    'site',
+    'source',
+    'contentBusId',
+    'duration',
+  ],
+  item: `<tr>
+    <td class="timestamp"></td>
+    <td class="status"></td>
+    <td class="method"></td>
+    <td class="route"></td>
+    <td class="path"></td>
+    <td class="user"></td>
+    <td class="site"></td>
+    <td class="source"></td>
+    <td class="contentBusId"></td>
+    <td class="duration"></td>
+  </tr>`,
+};
+
+function addErrorMessage(message, el) {
+  const errorDiv = document.createElement('div');
+  errorDiv.classList.add('log');
+  errorDiv.classList.add('error');
+  errorDiv.innerText = message;
+  el.insertAdjacentElement('afterend', errorDiv);
+}
+
+export async function saveGithubUrl() {
+  // clear previous logs
+  const logs = document.querySelectorAll('div.log');
+  logs.forEach((log) => log.remove());
+  const ghInput = document.getElementById('githuburl');
+  const ghUrl = ghInput.value;
+  console.log(ghUrl);
+  if (regexpFull.test(ghUrl) || regexpPartial.test(ghUrl)) {
+    const githubUrlEl = document.createElement('div');
+    githubUrlEl.classList.add('log');
+    githubUrlEl.classList.add('ghUrl');
+    githubUrlEl.innerText = ghUrl;
+    ghSubmit.insertAdjacentElement('afterend', githubUrlEl);
+    // Get logs
+    const values = await getLogs(ghUrl);
+    if (values && values.length > 0) {
+      githubUrlEl.classList.add('success');
+    } else {
+      githubUrlEl.classList.add('error');
+      addErrorMessage(`No logs found for ${ghUrl}`, ghSubmit);
+    }
+    // Build list
+    const logList = new List('logs', options, values);
+  } else {
+    addErrorMessage(`The Github URL does not look right.
+    Please enter the Github URL of the site you want to view logs for
+    Example: https://www.github.com/amol-anand/tools/tree/main
+    Example: www.github.com/amol-anand/tools
+    Example: github.com/amol-anand/tools`, ghSubmit);
+  }
+}
+
+function processGithubUrl(githubUrl) {
+  let extractedUrl;
+  if (githubUrl.includes('tree')) {
+    extractedUrl = githubUrl.match(regexpFull);
+  } else {
+    extractedUrl = githubUrl.match(regexpPartial);
+  }
+  const owner = extractedUrl[1];
+  const repo = extractedUrl[2];
+  let ref = 'main';
+  if (extractedUrl[3]) ref = extractedUrl[3];
+  return { owner, repo, ref };
+}
+
+async function getLogs(githubUrl) {
+  if (githubUrl === '') return [];
+  const { owner, repo, ref } = processGithubUrl(githubUrl);
+  console.log(`making a call to https://admin.hlx.page/log/${owner}/${repo}/${ref}`);
+  const resp = await fetch(`https://admin.hlx.page/log/${owner}/${repo}/${ref}`);
+  console.log(`resp is ${resp.status} and ${resp.statusText}`);
+  if (resp.status === 401) {
+    addErrorMessage(`401 Unauthorized. Please login to https://admin.hlx.page/login`, ghSubmit);
+    return [];
+  } 
+  if (!resp.ok) {
+    addErrorMessage(`Response not OK. Please login to https://admin.hlx.page/login`, ghSubmit);
+    return [];  
+  } else {
+    if (resp){
+      const logsJson = await resp.json();
+      if (logsJson) return logsJson.entries;
+    }
+  }
+  return [];
+}
+
+ghSubmit.addEventListener('click', saveGithubUrl);
