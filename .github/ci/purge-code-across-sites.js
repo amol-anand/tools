35078d804f60f1ab1a43a8fd0ee5d2dfb040531f
Amol Anand <amol.anand@gmail.com>
2024-09-25 21:02:11 -0400

    converting to array

--- .github/ci/purge-code-across-sites.js
+++ .github/ci/purge-code-across-sites.js
@@ -1,8 +1,8 @@
 // This file is used to purge the code across all sites
 try {
-  console.log(`env: ${process.env.ALL_CHANGED_FILES}`);
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  // console.log(`env: ${process.env.ALL_CHANGED_FILES}`);
+  const changedFiles = process.env.ALL_CHANGED_FILES.split(' ');
+  console.log(`changedFiles: ${changedFiles}`);
 } catch (error) {
   console.error(error.message);
 }
